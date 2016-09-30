8e4bf1b4dc87da5caf3023a965c34b8752c35ed4
Tobias Heer <tobias.heer@stud.hslu.ch>
2016-09-30 14:10:05 +0200

    changed common

--- INTRO_FRDM_kopie/Sources/main.c
+++ INTRO_FRDM_kopie/Sources/main.c
@@ -56,7 +56,10 @@ int main(void)
 
   /* Write your code here */
   /* For example: for(;;) { } */
-  APP_Start();
+  /**
+   *
+   *APP_Start();
+   */
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
