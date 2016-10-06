e89adf8f8d70ee567ac8fec10e31cea229c41a58
naegerr <robert.naeger@datazug.ch>
2016-10-06 16:16:29 +0200

    run-Ordner von Vorlesung ->Präprozessor

---
+++ INTRO_Robot_TORO/run/Sources/INTRO_Common_TORO/subdir.mk
@@ -0,0 +1,304 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Application.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Buzzer.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Debounce.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Drive.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Event.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/KeyDebounce.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Keys.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/LCD.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/LCDMenu.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/LED.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/LineFollow.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/MCP4728.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Maze.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Motor.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/NVM_Config.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Pid.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Platform.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/QuadCalib.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/RNet_App.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/RTOS.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Reflectance.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Remote.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Sem.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Shell.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/ShellQueue.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Tacho.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Timer.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Trigger.c \
+C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Turn.c 
+
+OBJS += \
+./Sources/INTRO_Common_TORO/Application.o \
+./Sources/INTRO_Common_TORO/Buzzer.o \
+./Sources/INTRO_Common_TORO/Debounce.o \
+./Sources/INTRO_Common_TORO/Drive.o \
+./Sources/INTRO_Common_TORO/Event.o \
+./Sources/INTRO_Common_TORO/KeyDebounce.o \
+./Sources/INTRO_Common_TORO/Keys.o \
+./Sources/INTRO_Common_TORO/LCD.o \
+./Sources/INTRO_Common_TORO/LCDMenu.o \
+./Sources/INTRO_Common_TORO/LED.o \
+./Sources/INTRO_Common_TORO/LineFollow.o \
+./Sources/INTRO_Common_TORO/MCP4728.o \
+./Sources/INTRO_Common_TORO/Maze.o \
+./Sources/INTRO_Common_TORO/Motor.o \
+./Sources/INTRO_Common_TORO/NVM_Config.o \
+./Sources/INTRO_Common_TORO/Pid.o \
+./Sources/INTRO_Common_TORO/Platform.o \
+./Sources/INTRO_Common_TORO/QuadCalib.o \
+./Sources/INTRO_Common_TORO/RNet_App.o \
+./Sources/INTRO_Common_TORO/RTOS.o \
+./Sources/INTRO_Common_TORO/Reflectance.o \
+./Sources/INTRO_Common_TORO/Remote.o \
+./Sources/INTRO_Common_TORO/Sem.o \
+./Sources/INTRO_Common_TORO/Shell.o \
+./Sources/INTRO_Common_TORO/ShellQueue.o \
+./Sources/INTRO_Common_TORO/Tacho.o \
+./Sources/INTRO_Common_TORO/Timer.o \
+./Sources/INTRO_Common_TORO/Trigger.o \
+./Sources/INTRO_Common_TORO/Turn.o 
+
+C_DEPS += \
+./Sources/INTRO_Common_TORO/Application.d \
+./Sources/INTRO_Common_TORO/Buzzer.d \
+./Sources/INTRO_Common_TORO/Debounce.d \
+./Sources/INTRO_Common_TORO/Drive.d \
+./Sources/INTRO_Common_TORO/Event.d \
+./Sources/INTRO_Common_TORO/KeyDebounce.d \
+./Sources/INTRO_Common_TORO/Keys.d \
+./Sources/INTRO_Common_TORO/LCD.d \
+./Sources/INTRO_Common_TORO/LCDMenu.d \
+./Sources/INTRO_Common_TORO/LED.d \
+./Sources/INTRO_Common_TORO/LineFollow.d \
+./Sources/INTRO_Common_TORO/MCP4728.d \
+./Sources/INTRO_Common_TORO/Maze.d \
+./Sources/INTRO_Common_TORO/Motor.d \
+./Sources/INTRO_Common_TORO/NVM_Config.d \
+./Sources/INTRO_Common_TORO/Pid.d \
+./Sources/INTRO_Common_TORO/Platform.d \
+./Sources/INTRO_Common_TORO/QuadCalib.d \
+./Sources/INTRO_Common_TORO/RNet_App.d \
+./Sources/INTRO_Common_TORO/RTOS.d \
+./Sources/INTRO_Common_TORO/Reflectance.d \
+./Sources/INTRO_Common_TORO/Remote.d \
+./Sources/INTRO_Common_TORO/Sem.d \
+./Sources/INTRO_Common_TORO/Shell.d \
+./Sources/INTRO_Common_TORO/ShellQueue.d \
+./Sources/INTRO_Common_TORO/Tacho.d \
+./Sources/INTRO_Common_TORO/Timer.d \
+./Sources/INTRO_Common_TORO/Trigger.d \
+./Sources/INTRO_Common_TORO/Turn.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Sources/INTRO_Common_TORO/Application.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Application.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Buzzer.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Buzzer.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Debounce.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Debounce.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Drive.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Drive.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Event.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Event.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/KeyDebounce.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/KeyDebounce.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Keys.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Keys.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/LCD.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/LCD.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/LCDMenu.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/LCDMenu.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/LED.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/LED.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/LineFollow.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/LineFollow.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/MCP4728.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/MCP4728.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Maze.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Maze.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Motor.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Motor.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/NVM_Config.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/NVM_Config.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Pid.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Pid.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Platform.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Platform.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/QuadCalib.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/QuadCalib.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/RNet_App.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/RNet_App.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/RTOS.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/RTOS.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Reflectance.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Reflectance.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Remote.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Remote.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Sem.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Sem.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Shell.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Shell.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/ShellQueue.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/ShellQueue.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Tacho.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Tacho.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Timer.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Timer.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Trigger.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Trigger.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Sources/INTRO_Common_TORO/Turn.o: C:/Users/rober_000/Documents/GitHub/robogit/INTRO_Common_TORO/Turn.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	echo -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
